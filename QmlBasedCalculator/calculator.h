e9e33b29905668f492c35a51a96db7f8ccc0b4ea
Ameer Salam <salamameer353@gmail.com>
2025-03-13 19:44:26 +0530

    V2 with better UI meathods and Backend code

--- QmlBasedCalculator/calculator.h
+++ QmlBasedCalculator/calculator.h
@@ -9,12 +9,7 @@ class calculator : public QObject{
 public:
     explicit calculator(QObject *parent=nullptr);
 
-    Q_INVOKABLE void digitPressed(double d);
-    Q_INVOKABLE void divide();
-    Q_INVOKABLE void multiply();
-    Q_INVOKABLE void subtract();
-    Q_INVOKABLE void add();
-    Q_INVOKABLE void equals();
+    Q_INVOKABLE void buttonPressed(char ch);
     Q_INVOKABLE void clear();
 };
 
