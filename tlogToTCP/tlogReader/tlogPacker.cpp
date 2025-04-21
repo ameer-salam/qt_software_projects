a4a8596c54149314c25a1ed7d259253c9b48a201
Ameer Salam <salamameer353@gmail.com>
2025-04-21 17:42:08 +0530

    Before laptop backup

--- tlogToTCP/tlogReader/tlogPacker.cpp
+++ tlogToTCP/tlogReader/tlogPacker.cpp
@@ -9,13 +9,14 @@ using namespace std;
 void TlogPacker::getTlogFileLocation(string IPFileAddress)
 {
     ifstream InputFile(IPFileAddress, ios::binary); //opens the file as binary
-    if(!InputFile.is_open())
+    if(InputFile.is_open())
     {
-        cout<<"The File Failed to open!"<<endl;
-        return;
+        cout<<"The file : " <<IPFileAddress<<"\t is open"<<endl;
+        cout<<"Starting the parsing operation!"<<endl;
     }
     else
     {
-        cout<<"The file : " <<IPFileAddress<<"\t is open"<<endl;
+        cout<<"The File Failed to open!"<<endl;
+        return;
     }
 }
